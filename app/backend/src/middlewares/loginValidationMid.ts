c0e200df56c92beae816cb89e3027e3b29f08c8c
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-03-29 18:22:09 -0300

    adequa mensagens do joi ao que pedem os requisitos

--- app/backend/src/middlewares/loginValidationMid.ts
+++ app/backend/src/middlewares/loginValidationMid.ts
@@ -2,9 +2,18 @@ import { Request, Response, NextFunction } from 'express';
 import * as Joi from 'joi';
 import StatusCodes from '../utils/StatusCodes';
 
+const notEmpty = 'All fields most be filled';
+
 export const loginSchema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required().min(7),
+  email: Joi.string().email().required().messages({
+    'string.required': notEmpty,
+    'string.email': 'Incorrect email or password',
+    'string.empty': notEmpty,
+  }),
+  password: Joi.string().required().min(7).messages({
+    'string.required': notEmpty,
+    'string.empty': notEmpty,
+  }),
 });
 
 const loginValidationMid = async (req:Request, res:Response, next:NextFunction) => {
