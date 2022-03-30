8217fec2db5e396978b9ff21138dfb8c02dbd966
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-03-30 15:52:38 -0300

    inicia testes

---
+++ app/backend/src/tests/user.test.ts
@@ -0,0 +1,26 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Users from '../database/models/Users'
+
+import { Response } from 'superagent'
+
+chai.use(chaiHttp);
+
+const {expect} = chai;
+
+describe('Testa algo', () => {
+  it('deveria fazer algo', () => {
+
+  })
+
+  it('deveria fazer algo', () => {
+    
+  })
+
+  it('deveria fazer algo', () => {
+    
+  })
+})
