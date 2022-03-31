9d193b1bf4e3932a58156a73815a37bd86dca08f
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-03-31 20:34:32 -0300

    fix: middleware export

--- app/backend/src/middlewares/clubIdMiddleware.ts
+++ app/backend/src/middlewares/clubIdMiddleware.ts
@@ -2,11 +2,11 @@ import { Request, Response, NextFunction } from 'express';
 import Joi = require('joi');
 import StatusCodes from '../utils/StatusCodes';
 
-export const idSchema = Joi.object({
+const idSchema = Joi.object({
   id: Joi.number().required(),
 });
 
-export const clubIdMiddleware = async (req:Request, res:Response, next:NextFunction) => {
+const clubIdMiddleware = async (req:Request, res:Response, next:NextFunction) => {
   const { id } = req.params;
 
   const { error } = idSchema.validate({ id });
