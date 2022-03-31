fdbb62366fd5738a64a68780f77d09d1b2c93301
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-03-31 03:25:47 -0300

    Implementa teste do caso de sucesso no metodo post do endpoint /login

---
+++ app/backend/src/tests/mocks/userMocks.ts
@@ -0,0 +1,24 @@
+import { string } from "joi"
+
+export const foundUserMock = {
+  id: 1,
+  username: "Admin",
+  role: "admin",
+  password: "admin_password",
+  email: "admin@admin.com",
+}
+
+export const loginResponseMock = {
+  user: {
+    id: foundUserMock.id,
+    username: foundUserMock.username,
+    role: foundUserMock.role,
+    email: foundUserMock.email,
+  },
+  token: 'eyJhb',
+}
+
+export const accurateRequestInfo = {
+  email: "admin@admin.com",
+  password: "admin_password",
+}
--- app/backend/src/tests/user.test.ts
+++ app/backend/src/tests/user.test.ts
@@ -1,26 +1,62 @@
 import * as sinon from 'sinon';
 import * as chai from 'chai';
+import bcryptjs = require('bcryptjs');
 import chaiHttp = require('chai-http');
 
 import { app } from '../app';
 import Users from '../database/models/Users'
 
-import { Response } from 'superagent'
+import { Response } from 'superagent';
+import { foundUserMock, loginResponseMock, accurateRequestInfo } from './mocks/userMocks';
+import { assert } from 'console';
 
 chai.use(chaiHttp);
 
 const { expect } = chai;
 
-describe('Testa algo', () => {
-  it('deveria fazer algo', () => {
-
+describe('Em caso de sucesso no método POST do endpoint /login:', () => {
+  let chaiHttpRes:Response 
+  
+  
+  before(async () => {
+    sinon.stub(Users, 'findOne').resolves(foundUserMock as Users);
+    sinon.stub(bcryptjs, 'compare').resolves(true);
   })
 
-  it('deveria fazer algo', () => {
+  after(async () => {
+    (Users.findOne as sinon.SinonStub).restore();
+    (bcryptjs.compare as sinon.SinonStub).restore();
+  })
+
+  it('Deveria retornar status 200', async () => {
+    chaiHttpRes = await chai.request(app).post('/login').send(accurateRequestInfo);
+    const { status } = chaiHttpRes;
+    expect(status).to.equal(200);
+  })
+
+  it('O id do usuário encontrado deve ser igual ao id esperado', async () => {
+    chaiHttpRes = await chai.request(app).post('/login').send(accurateRequestInfo);
+    const { id } = chaiHttpRes.body.user;
+    expect(id).to.be.equal(loginResponseMock.user.id);
     
   })
 
-  it('deveria fazer algo', () => {
-    
+  it('O username do usuário encontrado deve ser igual ao username esperado', async () => {
+    chaiHttpRes = await chai.request(app).post('/login').send(accurateRequestInfo);
+    const { username } = chaiHttpRes.body.user;
+    expect(username).to.be.equal(loginResponseMock.user.username);
   })
+
+  it('A role do usuário encontrado deve ser igual a role esperada', async () => {
+    chaiHttpRes = await chai.request(app).post('/login').send(accurateRequestInfo);
+    const { role } = chaiHttpRes.body.user;
+    expect(role).to.be.equal(loginResponseMock.user.role);
+  })
+
+  it('O email do usuário encontrado deve ser igual ao email esperado', async () => {
+    chaiHttpRes = await chai.request(app).post('/login').send(accurateRequestInfo);
+    const { email } = chaiHttpRes.body.user;
+    expect(email).to.be.equal(loginResponseMock.user.email);
+  })
+
 })
