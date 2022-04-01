51bd66cf813fddcf6174ba10c6750ebd278c3b90
JoaoHenriqueAlmeida <joaoh.almeidaaraujo@gmail.com>
2022-03-31 23:25:44 -0300

    services layer get /matches

---
+++ app/backend/src/services/MatchServices.ts
@@ -0,0 +1,36 @@
+import Matches from '../database/models/Matches';
+import Clubs from '../database/models/Clubs';
+
+import responseGenerator from '../utils/resGenerator';
+import StatusCodes from '../utils/StatusCodes';
+
+export const getMatchesArray = async () => {
+  const matchesArray = await Matches.findAll({
+    include: [
+      { model: Clubs, as: 'homeClubs', attributes: { exclude: ['id'] } },
+      {},
+    ],
+  });
+
+  if (!matchesArray.length) {
+    return responseGenerator(StatusCodes.NotFound, 'Coud not find any Matches');
+  }
+
+  return responseGenerator(StatusCodes.OK, '', matchesArray);
+};
+
+export const getInProgressMatches = async (inProgress: boolean) => {
+  const matchesArray = await Matches.findAll({
+    where: { inProgress },
+    include: [
+      { model: Clubs, as: 'homeClubs', attributes: { exclude: ['id'] } },
+      {},
+    ],
+  });
+
+  if (!matchesArray.length) {
+    return responseGenerator(StatusCodes.NotFound, 'Coud not find any Matches');
+  }
+
+  return responseGenerator(StatusCodes.OK, '', matchesArray);
+};
